3dddaaaf57a8cf9796097777e9a837eb60cbd8b3
Natalia Marukovich <nmarukovich@gmail.com>
2022-04-20 12:59:41 +0400

    some fixes

--- multi-cluster-service/gke/setup-gke.sh
+++ multi-cluster-service/gke/setup-gke.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
 prefix=${1:-psmdb}
 
+for binary in gcloud kubectl
+do
+  command -v $binary >/dev/null 2>&1 || { echo >&2 "The script requires $binary but it's not installed.  Aborting."; exit 1; }
+done
 gcloud container clusters create $prefix-mcs-1 \
 --region=europe-west1 \
 --enable-ip-alias \
@@ -9,7 +14,7 @@ gcloud container clusters create $prefix-mcs-1 \
 --machine-type n1-standard-4 \
 --num-nodes=1
 
-kubectl create namespace mcs && kubens mcs
+kubectl create namespace mcs && kubectl config set-context --current --namespace mcs
 
 gcloud container clusters create $prefix-mcs-2 \
 --region=europe-west2 \
@@ -20,7 +25,7 @@ gcloud container clusters create $prefix-mcs-2 \
 --machine-type n1-standard-4 \
 --num-nodes=1
 
-kubectl create namespace mcs &&  kubens mcs
+kubectl create namespace mcs &&  kubectl config set-context --current --namespace mcs
 
 gcloud container hub memberships register $prefix-mcs-1 \
 --gke-cluster europe-west1/$prefix-mcs-1 \
